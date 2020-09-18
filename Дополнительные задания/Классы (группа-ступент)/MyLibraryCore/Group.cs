357fa7bcee639315232c188c4512be7ea32665ef
horoshiy2k2 <antonio.horoshiy@gmail.com>
2020-09-18 13:36:25 +0300

    dop5 second commit

---
+++ Дополнительные задания/Классы (группа-ступент)/MyLibraryCore/Group.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibraryCore
+{
+    public class Group
+    {
+        List<Student> _studentsList = new List<Student> { };
+
+        // Конструктор. Принимает список объектов
+        public Group(List<Student> students)
+        {
+            _studentsList.AddRange(students);
+        }
+
+        public void PrintMiddleScoreGroup()
+        {
+            Console.WriteLine("\nMiddleScores:\n");
+
+            for (int i = 0; i < _studentsList.Count; i++)
+            {
+                var tempStudent = _studentsList[i];
+
+                Console.WriteLine("Student {0}: {1:f2}", tempStudent.Name, tempStudent.MiddleScore);
+            }
+            
+        }
+
+        public void PrintScoresGroup()
+        {
+            Console.WriteLine("\nScores:\n");
+
+            for (int i = 0; i < _studentsList.Count; i++)
+            {
+                var tempStudent = _studentsList[i];
+
+                Console.Write("Student {0}: ", tempStudent.Name);
+                tempStudent.PrintScores();
+            }
+        }
+    }
+}
+
+//2. класс Группа
+//Поля: список студентов
+//Методы: вывести средний балл каждого студента, вывести оценки каждого студента
+//можно метод, добавляющий студента - та ну
---
+++ Дополнительные задания/Классы (группа-ступент)/MyLibraryCore/Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace MyLibraryCore
+{
+    public class Student
+    {
+        // Обычный конструктор
+        public Student(string name, int[] scores)
+        {
+            _name = name;
+            _scores = scores;
+            CalculateAverageScore();
+        }
+
+        // Случайные оценки студента с именем name
+        public Student(string name) 
+        {
+            var rand = new Random();
+            _name = name;
+            _scores = new int[rand.Next(1, 11)]; // 1-10 оценок
+
+            for (int i = 0; i < _scores.Length; i++)
+            {
+                _scores[i] = rand.Next(0, 11);
+            }
+
+            CalculateAverageScore();
+        }
+
+        private string _name; // в данном случае можно было обойтись автосвойством {get; private set}
+
+        public string Name
+        {
+            get { return _name; }
+            private set { _name = value; }
+        }
+
+        private int[] _scores;
+
+        public double MiddleScore { get; private set; }
+
+        public void CalculateAverageScore()
+        {
+            MiddleScore = (double)_scores.Sum() / _scores.Length;// проверить, как считает
+        }
+
+        public void PrintScores()
+        {
+            foreach (var score in _scores)
+            {
+                Console.Write(score + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
+//Реализовать в отдельной сборке
+//1.класс Студент
+//Поля: имя, фамилия, оценки(массив int)
+//Методы: почитать средний балл, вывести оценки студента
