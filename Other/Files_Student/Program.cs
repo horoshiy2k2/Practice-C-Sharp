abb26147da72f25d1925c7b75013e619d3989c20
horoshiy2k2 <antonio.horoshiy@gmail.com>
2020-10-18 12:39:51 +0300

    Files commit 1

---
+++ Other/Files_Student/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Files_Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new List<Student>();
+
+            // чтение из файла
+            // отдельный метод сделать
+            var path = @"D:\CS\Students Results\Results.txt";
+            using (FileStream fstream = File.OpenRead(path)) // а если такого пути не существует?
+            {
+                byte[] array = new byte[fstream.Length];
+                fstream.Read(array, 0, array.Length);
+                string textFromFile = System.Text.Encoding.Default.GetString(array); // из байтового представления сделали строковое
+
+                var lines = textFromFile.Split('\n'); // по строкам разбил
+
+                var studentsInfo = new List<string>();
+                foreach (var line in lines)
+                {
+                    var data = line.Split('\t');
+                    students.Add(new Student(data[0], data[1], int.Parse(data[2]), int.Parse(data[3]), int.Parse(data[4]), data[5][0] == 'b'));
+                }
+                Console.WriteLine($"Файл \"{path}\" прочитан") ;
+            }
+
+            var text = "";
+            foreach (var student in students)
+            {
+                text += student.ToString();
+            }
+
+            var pathResults = @"D:\CS\Students Results\Scholarship.txt";
+            using (FileStream fstream = File.OpenWrite(pathResults))
+            {
+                // преобразуем строку в байты
+                byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                // запись массива байтов в файл
+                fstream.Write(array, 0, array.Length);
+                Console.WriteLine($"Результаты записаны в файл \"{pathResults}\"");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Other/Files_Student/Student.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Files_Students
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public int Math { get; set; }
+        public int Phys { get; set; }
+        public int Prog { get; set; }
+        public bool IsScholarship { get; set; }
+
+        private double _average;
+
+        public double Average
+        {
+            get { return _average; }
+            private set { _average = value; }
+        }
+
+        static double baseScholarship = 77.08;
+
+        private double _scholarship;
+
+        public double Scholarship
+        {
+            get { return _scholarship; }
+            private set { _scholarship = value; }
+        }
+
+        public Student(string name, string group, int math, int phys, int prog, bool isScholarship)
+        {
+            Name = name;
+            Group = group;
+            Math = math; // можно словарь предмет - оценка
+            Phys = phys;
+            Prog = prog;
+            IsScholarship = isScholarship;
+            CalculateAverage();
+            CalculateScholarship();
+        }
+
+        public void CalculateAverage()
+        {
+            Average = (Math + Phys + Prog) / 3.0; // если словарём, тогда тут нужно делить на количество предметов
+        }
+
+        public void CalculateScholarship()
+        {
+            switch ((int)Average)
+            {
+                case 5:
+                    Scholarship = baseScholarship * 1;
+                    break;
+                case 6:
+                    Scholarship = baseScholarship * 1.1;
+                    break;
+                case 7:
+                    Scholarship = baseScholarship * 1.2;
+                    break;
+                case 8:
+                    Scholarship = baseScholarship * 1.4;
+                    break;
+                case 9:
+                case 10:
+                    Scholarship = baseScholarship * 1.6;
+                    break;
+                default:
+                    Scholarship = 0;
+                    break;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}\t{Average:f2}\t{Scholarship:f2}р\n";
+        }
+    }
+}
