faf16e506042200ef6fb9ef19fdfa357ff11965d
horoshiy2k2 <antonio.horoshiy@gmail.com>
2020-10-23 10:15:59 +0300

    HW5 commit 1

---
+++ CurseCS/Advanced level/Homework5/ConverterTemperature.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public static class ConverterTemperature
+    {
+
+        public static double CurrentFahrenheitValue { get; set; }
+        public static double CurrentСelsiusValue { get; set; }
+
+        public static void FromСelsiusToFahrenheit(double tC)
+        {
+            CurrentFahrenheitValue = (tC * 9 / 5) + 32; 
+        }
+
+        public static void FromFahrenheitToСelsius(double tF)
+        {
+            CurrentСelsiusValue = (tF - 32) * 5 / 9;
+        }
+        
+    }
+}
+
+//(25 °C × 9 / 5) +32 = 77 °F
+// - (100 °F − 32) × 5 / 9 = 37,778 °C
---
+++ CurseCS/Advanced level/Homework5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace Homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly asm = Assembly.LoadFrom(@"D:\MyLibrary.dll");
+            Console.WriteLine(asm.FullName);
+
+            var typeAsm = asm.GetType("MyLibrary.ConverterTemperature");
+
+            var propF = typeAsm.GetProperty("CurrentFahrenheitValue");
+            var propC = typeAsm.GetProperty("CurrentСelsiusValue");
+
+            var methodCtoF = typeAsm.GetMethod("FromСelsiusToFahrenheit");
+            var methodFtoC = typeAsm.GetMethod("FromFahrenheitToСelsius");
+
+            //т.к. класс статический, то не нужно создавать экземпляр: Activator.CreateInstance(...)
+
+            methodCtoF.Invoke(null, new object[] { 25 });
+            methodFtoC.Invoke(null, new object[] { 100 });
+
+            Console.WriteLine(propF.GetValue(null));
+            Console.WriteLine(propC.GetValue(null));
+        }
+    }
+}
